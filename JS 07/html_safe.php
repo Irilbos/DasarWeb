c898d5c2e2aecd13194299abc16e9f05df97accb
= <=>
2024-10-08 08:48:40 +0700

    praktikum 4/js7

---
+++ JS 07/html_safe.php
@@ -0,0 +1,10 @@
+<?php
+$input = $_POST['input'];
+$input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+$email = $_POST['email'];
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+} else {
+    echo "Input tidak ditemukan.";
+}
+?>

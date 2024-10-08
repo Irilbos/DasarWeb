ddac07ff8b630d860bcd8c40ba7f6e337e072ae5
= <=>
2024-10-08 08:36:12 +0700

    Praktikum 3/js7

---
+++ JS 07/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <title> From Input PHP</title>
+    <head>
+        <body>
+            <h2> Form Input PHP</h2>
+            <form method="post" action="proses_form.php">
+                <label for="nama">Nama:</label>
+                <input type="text" name="nama" id="email" required> <br><br>
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email" required><br><br>
+                <input type="submit" name="submit" value="submit">
+            </form>
+        </body>
+    </head>
+</html>

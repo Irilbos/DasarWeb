3442efd54ebf7567bb201f3a16c9124ae304c591
= <=>
2024-10-15 10:59:39 +0700

    practical 4/js8

--- JS 08/form_upload_ajax.php
+++ JS 08/form_upload_ajax.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <link rel="stylesheet" type="text/css" href="upload.css">
   <title>Unggah File Dokumen</title>
-  <style>
-
-  </style>
 </head>
 <body>
-  <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
-    <label for="file">Pilih File:</label>
-    <input type="file" name="file" id="file" accept=".pdf,.docx,.xlsx">
-    <input type="submit" value="Unggah">
-  </form>
-  <div id="status"></div>
-
+  <div class="upload-form-container">
+    <h2>Unggah File Dokumen</h2>
+    <form id="upload-form" action="upload.php"   
+ method="post" enctype="multipart/form-data">
+      <div class="file-input-container">
+        <input type="file" name="file" id="file" class="file-input">
+        <label for="file" class="file-label">Pilih File</label>
+      </div>
+      <button type="submit" name="submit" class="upload-button" id="upload-button" disabled>Unggah</button>
+    </form>
+    <div id="status" class="upload-status"></div>
+  </div>
   <script src="https://code.jquery.com/jquery-1.14.0.min.js"></script>
   <script src="upload.js"></script>
 </body>
